32a996232bcbaf70d61084eab9841cd0321b02a9
Joseph Alba <jalba@egov.com>
2011-10-27 22:32:58 -0400

    Fixed some silliness

--- lib/gemline.rb
+++ lib/gemline.rb
@@ -4,18 +4,32 @@ require 'net/http'
 class Gemline
   VERSION = "0.0.1"
 
-  gem = ARGV[0].to_s.gsub(/[^\w\-]+/,'')
-  doc = Net::HTTP.get(URI.parse("http://rubygems.org/api/v1/gems/#{gem}.json"))
-  response = Crack::JSON.parse(doc)
-  gemline = %Q{gem "#{gem}", "~> #{response['version']}"}  
-
-  puts gemline
-
-  begin
-    IO.popen('pbcopy', 'r+') do |clipboard|
-      clipboard.puts gemline
-      puts "  Gem line copied to your clipboard.  Ready to paste into your Gemfile"
+  def self.start
+    gem = ARGV[0].to_s.gsub(/[^\w\-]+/,'')
+    if (gem.empty?)
+      puts "Usage: gemline [GEM NAME]"
+      exit
     end
-  rescue
+  
+    doc = Net::HTTP.get(URI.parse("http://rubygems.org/api/v1/gems/#{gem}.json"))
+
+    if doc.match(/could not be found/)
+      puts "Rubygem #{gem} was not found on rubygems.org"
+      exit
+    else
+      response = Crack::JSON.parse(doc)
+      gemline = %Q{gem "#{gem}", "~> #{response['version']}"}  
+
+      puts gemline
+
+      begin
+        if clipboard = IO.popen('pbcopy', 'r+')
+          clipboard.puts gemline
+          puts "  Gem line copied to your clipboard.  Ready to paste into your Gemfile"
+        end
+      rescue
+      end
+    end
   end
+
 end
