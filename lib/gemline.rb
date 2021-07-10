edff27d87a66baad0d7037343ff22c4c97eb7873
Joe Alba <joe@joealba.com>
2021-07-10 11:26:57 -0400

    Remove comment

--- lib/gemline.rb
+++ lib/gemline.rb
@@ -41,7 +41,6 @@ class Gemline
     uri = URI.parse("https://rubygems.org/api/v1/gems/#{gem_name}.json")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
     response.body
